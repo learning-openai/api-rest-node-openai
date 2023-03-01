bb027e3431873d0ba6ad270693e1c7be5aa83b49
GonzaloEspinoza <gonis1990@gmail.com>
2023-03-01 12:40:23 -0400

    refactor(refacoring): received message meta

--- src/routes/api/v0.1/meta_whatsapp/metaWhtsapp.js
+++ src/routes/api/v0.1/meta_whatsapp/metaWhtsapp.js
@@ -23,7 +23,25 @@ class MetaWhatsapp{
     static async receiveMetaWhatsappData(req, res, next){
 
         console.log('--- post received data meta ---')
-        console.log(req)
+        // console.log(req)
+        // -los datos vieneen en el body --> req.body
+        // body: { object: 'whatsapp_business_account', entry: [ [Object] ] },...
+        
+        const data = req?.body;
+        const phoneNumberClient = data["entry"][0]["changes"][0]["value"]["messages"][0]["from"]
+        const message = data["entry"][0]["changes"][0]["value"]["messages"][0]["text"]["body"]
+        const idWAMessage = data["entry"][0]["changes"][0]["value"]["messages"][0]["id"]
+        const timestamp = data["entry"][0]["changes"][0]["value"]["messages"][0]["timestamp"]
+
+        console.log({
+            phoneNumberClient,
+            message,
+            idWAMessage,
+            timestamp
+        })
+
+        res.status(200).send({status:"success"})
+
         // res.status(200).send('reciceved data');
     }
 }
