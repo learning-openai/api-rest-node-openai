99a60e5dfd4e1751f1418db231f4af0ba3b32b2e
GonzaloEspinoza <gonis1990@gmail.com>
2023-03-07 09:21:06 -0400

    release(model): add mdel user and connnected db

--- src/database/model/product.model.js
+++ src/database/model/product.model.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const Product = new Schema({
-
+    name:String,
+    price:Number,
+    barnd:String,
+    stock:Number,
+    color:String,
+    category:String,
+    description:String,
+    embedding:[],
 })
 
 
